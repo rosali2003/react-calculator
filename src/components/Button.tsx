e1b5622148fc5d6f44a1485c2facb9d9b4162e5a
Rosa Li <rosali.tks@gmail.com>
2023-06-06 15:26:56 -0400

    convert to typescript

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,13 @@
 import "./Button.css";
 import React from 'react';
 
-export const Button = ({className, value, onClick}) => {
+interface Props {
+  className: string;
+  value: string;
+  onClick: () => void
+}
+
+export const Button: React.FC<Props> = ({className, value, onClick}) => {
   return (
     <button style={{border: 'none'}} className={className} onClick={onClick}>{value}</button>
   )
